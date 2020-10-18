e80ddc493b644bad1dd3bc292c5724763b87fa1c
gao <1055652678@qq.com>
2020-10-18 12:08:07 +0800

    feat

---
+++ scripts/generate-api.config.js
@@ -0,0 +1,8 @@
+// 可以扩展其他的配置，但是目前还没想到，，，鹅鹅鹅饿
+const options = {
+    APIGROUP: [
+        'http://127.0.0.1:4523/export/openapi?projectId=349052&specialPurpose=openapi-generator&apiDetailId=3576699', // 油品
+    ]
+}
+
+module.exports = options
